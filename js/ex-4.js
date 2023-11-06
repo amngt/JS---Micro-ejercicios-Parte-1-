c1d3e48a63e303d2856565f1dd338ec42691ff85
Andrea Mingot <andreamingot@gmail.com>
2023-11-07 00:01:52 +0100

    cuarto ejercicio

--- js/ex-4.js
+++ js/ex-4.js
@@ -1,2 +1,14 @@
 /*ejercicio 4*/
 /*Escribe un programa que pida dos números y escriba “La suma de <numero-uno> con <numero-dos> es <resultado>”*/
+//llamo al documento (document.)y le pido el input que tiene "primero" y "segundo"
+var miPrimero = document.querySelector ("#primero")
+var miSegundo = document.querySelector ("#segundo")
+
+//tengo que llamar la función onclick "sumar" 
+//parseInt(cadena) convierte una cadena en número
+function sumar() { 
+    divResultado.innerHTML = parseInt(miPrimero.value) + parseInt(miSegundo.value) 
+
+}
+//muestra el resultado de la suma
+var divResultado = document.querySelector ("#resultado")
